54a98ffb5d0566544f7377ae17cdb13e34b74580
Vidugavia <49245323+Vidugavia@users.noreply.github.com>
2020-05-06 18:20:01 +0200

    Zatrzymanie odmierzania czasu po wciśnięciu przycisku pauza

--- Core/Inc/game.h
+++ Core/Inc/game.h
@@ -13,6 +13,8 @@ float accX, accY, accZ, out[4];
 int time=0;
 char cstr[16];
 
+TIM_HandleTypeDef htim3;
+
 void buttons(uint16_t GPIO_Pin){
 	switch (GPIO_Pin){
 		case 2: //przycisk 1 --UP
@@ -57,6 +59,8 @@ void buttons(uint16_t GPIO_Pin){
 				case 2:
 					place_in_game=2;
 					ILI9341_FillScreen(background);
+					time=0;
+					HAL_TIM_Base_Start_IT(&htim3);
 					return;
 					break;
 				}
@@ -78,6 +82,7 @@ void buttons(uint16_t GPIO_Pin){
 				break;
 			case 2:
 				//zapauzowanie gry
+				HAL_TIM_Base_Stop_IT(&htim3);
 				return;
 				break;
 			case 3:
