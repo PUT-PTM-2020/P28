d093e0f408cfd43d8ed196e001c1f550003424f5
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2020-05-25 15:55:33 +0200

    Obsługa krawędzi

--- Core/Inc/acc_ball.h
+++ Core/Inc/acc_ball.h
@@ -14,38 +14,44 @@
 int pozycja_x=0;
 int pozycja_y=0;
 
+//(0,0)->pierwszy piksel
+//X(120,0)->ostatnia pozycja w prawo->(110,0)
+//-X(-119,0)->ostatnia pozycja w lewo->(-109,0)
+//Y(0,150)->ostatnia pozycja w góre->(0,140)
+//-Y(0,-149)->ostatnia pozycja w góre->(0,-139)
+
 void acc_ball(float accX, float accY){
 
-	if(accX>=30)
+	if(accX>=30 && pozycja_x<110)
 	{
 		pozycja_x+=1;
 	}
-	if(accY>=30)
+	if(accY>=30 && pozycja_y<140)
 	{
 		pozycja_y+=1;
 	}
-	if(accX<=-30)
+	if(accX<=-30 && pozycja_x>-109)
 	{
 		pozycja_x-=1;
 	}
-	if(accY<=-30)
+	if(accY<=-30 && pozycja_y>-139)
 	{
 		pozycja_y-=1;
 	}
 
-	if(accX>=350)
+	if(accX>=350 && pozycja_x<110)
 	{
 		pozycja_x+=3;
 	}
-	if(accY>=350)
+	if(accY>=350 && pozycja_y<140)
 	{
 		pozycja_y+=3;
 	}
-	if(accX<=-350)
+	if(accX<=-350 && pozycja_x>-109)
 	{
 		pozycja_x-=3;
 	}
-	if(accY<=-350)
+	if(accY<=-350 && pozycja_y>-139)
 	{
 		pozycja_y-=3;
 	}
