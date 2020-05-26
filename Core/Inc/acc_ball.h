3dd660a340a0c1646e1072137084e383947ce83f
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2020-05-26 18:45:56 +0200

    Update pozycja piłki

--- Core/Inc/acc_ball.h
+++ Core/Inc/acc_ball.h
@@ -13,45 +13,46 @@
 
 int pozycja_x=0;
 int pozycja_y=0;
-
+int start_x=18;
+int start_y=18;
 //(0,0)->pierwszy piksel
-//X(120,0)->ostatnia pozycja w prawo->(110,0)->minus ściana(107,0)
-//-X(-119,0)->ostatnia pozycja w lewo->(-109,0)->minus ściana(-106,0)
-//Y(0,150)->ostatnia pozycja w góre->(0,140)->minus ściana i dół(0,137)
-//-Y(0,-149)->ostatnia pozycja w góre->(0,-139)->minus ściana i dół(0,-96)
+//X(120,0)->ostatnia pozycja w prawo->(110,0)->minus ściana(99,0)
+//-X(-119,0)->ostatnia pozycja w lewo->(-109,0)->minus ściana(-102,0)
+//Y(0,150)->ostatnia pozycja w góre->(0,140)->minus ściana i dół(0,142)
+//-Y(0,-149)->ostatnia pozycja w góre->(0,-139)->minus ściana i dół(0,-100)
 
 void acc_ball(float accX, float accY){
 
-	if(accX>=30 && pozycja_x<99)
+	if(accX>=30 && pozycja_x<219)
 	{
 		pozycja_x+=1;
 	}
-	if(accY>=30 && pozycja_y<142)
+	if(accY>=30 && pozycja_y<260)
 	{
 		pozycja_y+=1;
 	}
-	if(accX<=-30 && pozycja_x>-102)
+	if(accX<=-30 && pozycja_x>18)
 	{
 		pozycja_x-=1;
 	}
-	if(accY<=-30 && pozycja_y>-100)
+	if(accY<=-30 && pozycja_y>18)
 	{
 		pozycja_y-=1;
 	}
 
-	if(accX>=350 && pozycja_x<100)
+	if(accX>=350 && pozycja_x<219)
 	{
 		pozycja_x+=3;
 	}
-	if(accY>=350 && pozycja_y<142)
+	if(accY>=350 && pozycja_y<260)
 	{
 		pozycja_y+=3;
 	}
-	if(accX<=-350 && pozycja_x>-102)
+	if(accX<=-350 && pozycja_x>18)
 	{
 		pozycja_x-=3;
 	}
-	if(accY<=-350 && pozycja_y>-100)
+	if(accY<=-350 && pozycja_y>18)
 	{
 		pozycja_y-=3;
 	}
@@ -62,7 +63,7 @@ void display_ball(float accX, float accY){
 
 	acc_ball(accX,accY);
 
-	ILI9341_DrawFilledCircle(ILI9341_WIDTH / 2 + pozycja_x, ILI9341_HEIGHT / 2 - pozycja_y, 10, 10);
+	ILI9341_DrawFilledCircle(start_x + pozycja_x, start_Y - pozycja_y, 10, 10);
 
 }
 
