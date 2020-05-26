b5adf14c0e9744b46f4e0c1f1235db123d7805e2
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2020-05-26 19:50:36 +0200

    Revert "Revert "Add left_wall""
    This reverts commit 1af653332b365d3078e08b6d8ef5284ab99877b4.

--- Core/Inc/acc_ball.h
+++ Core/Inc/acc_ball.h
@@ -10,6 +10,7 @@
 
 #include "ili9341.h"
 #include "lis3dsh.h"
+#include "maze.h"
 
 int pozycja_x=0;
 int pozycja_y=0;
@@ -20,22 +21,45 @@ int start_y=18;
 //-X(-119,0)->ostatnia pozycja w lewo->(-109,0)->minus ściana(-102,0)
 //Y(0,150)->ostatnia pozycja w góre->(0,140)->minus ściana i dół(0,142)
 //-Y(0,-149)->ostatnia pozycja w góre->(0,-139)->minus ściana i dół(0,-100)
+bool left_wall(float pozycja_x, float pozycja_y){
+	if(pozycja_x >0 && pozycja_y < 40)//pierwszy wiersz
+	{
+		
+	}
+	if(40 < pozycja_x < 200  && 40 < pozycja_y < 240)//środek
+	{
+			
+	}
+	if(pozycja_x > 0 && 240 < pozycja_y < 280)//ostatni wiersz
+	{
+			
+	}
+	if(pozycja_x < 40  && 40 < pozycja_y < 240)//pierwsza kolumna
+	{
+				
+	}
+	if(pozycja_x > 200  && 40 < pozycja_y < 240)//ostatnia kolumna
+	{
+					
+	}
+	
 
-void acc_ball(float accX, float accY){
+}
+void acc_ball(float pozycja_x, float pozycja_y){
 
-	if(accX>=30 && pozycja_x<219)
+	if(accX>=30 && pozycja_x<219 && )//w prawo
 	{
 		pozycja_x+=1;
 	}
-	if(accY>=30 && pozycja_y<260)
+	if(accY>=30 && pozycja_y<260)//w góre
 	{
 		pozycja_y-=1;
 	}
-	if(accX<=-30 && pozycja_x>18)
+	if(accX<=-30 && pozycja_x>18)//w lewo
 	{
 		pozycja_x-=1;
 	}
-	if(accY<=-30 && pozycja_y>18)
+	if(accY<=-30 && pozycja_y>18)//w dół
 	{
 		pozycja_y+=1;
 	}
